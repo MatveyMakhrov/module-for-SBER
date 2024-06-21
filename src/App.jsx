1eb9b4472435b3e9c55db977ee1bf5c6cb8c6036
MakhrovMatvey <dgdrg10@mail.ru>
2024-06-22 01:54:15 +0300

    new fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { createRef, forwardRef, useRef, useState } from 'react';
+import React, { createRef, forwardRef, useRef, useState, useEffect } from 'react';
 import { createAssistant, createSmartappDebugger, CharacterId } from '@salutejs/client';
 import styled from "styled-components";
 import logo100 from './res/logo100-transformed.png';
@@ -40,6 +40,7 @@ let TrueAnswer = 'test';
 let question = '';
 async function fetchQuestionAndSetState(callback) {
   try {
+    
     let response = await fetch('https://4-gk.ru/api/v1/question/random');
     let data = await response.json();
     TrueAnswer = data.question.answer;
@@ -51,20 +52,22 @@ async function fetchQuestionAndSetState(callback) {
   }
 }
 
+const StyledButtonOutputComponentGreen = styled.button `border:3px solid #18ab29;`;
+const StyledButtonOutputComponentRed = styled.button `border:3px solid red;`;
+
 const ButtonOutputComponent = forwardRef(({ onClick, pos_y }, ref) => {
   if (pos_y === 2) {
     return (
       <div className='saluteQuestionButton'>
-        <Button outlined focused contentLeft={<IconPlusCircle />}  onClick={onClick}  ref={(pos_y === 2) ? ref : null } text="Выдай вопрос"></Button>
-        <output className="output-text">{onClick.outputText}</output>
+        <Button focused contentLeft={<IconPlusCircle />}  onClick={onClick}  ref={(pos_y === 2) ? ref : null } text="Выдай вопрос"></Button>
       </div>
     );
   }
   else{
     return (
         <div className='saluteQuestionButton'>
-          <Button contentLeft={<IconPlusCircle />} onClick={onClick}  ref={(pos_y === 2) ? ref : null} text="Выдай вопрос"></Button>
-          <output className="output-text">{onClick.outputText}</output>
+          <Button outlined={false} contentLeft={<IconPlusCircle />} text="Выдай вопрос"></Button>
+          
         </div>
       );
   } 
@@ -141,7 +144,7 @@ const InfoButton = forwardRef(( { pos_y }, ref ) => {
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
-  
+
   if (pos_y === 3) {
     return (
       <div onClick={toggleMenu} className='container'>
@@ -157,17 +160,19 @@ const InfoButton = forwardRef(( { pos_y }, ref ) => {
             <p>2. Проверить ответ можно нажав на поле ввода "Введи ответ", написать текст и отправить его на кнопку "ОК", или словами: "Мой ответ".</p>
             <p>3. Если вы не знаете ответ, то можно нажать на кнопку "Сдаться", и выведется правильный ответ.</p>
             <h3>Желаю удачи!</h3>
-            <h4>Чтобы закрыть инструкцию, необходимо нажать "ОК".</h4>
+            <h4>Чтобы закрыть инструкцию, необходимо нажать "ОК"</h4>
             </Body1>
            </Sheet>
         )}
+      
       </div>
     );
   } 
   else {
     return (
-      <div className='container' onClick={toggleMenu}>
-        <Button pin = 'circle-circle' className='saluteInfoButton' contentLeft={<IconInfo />} id="button2" ref={pos_y === 3 ? ref : null} outlined = {false}></Button>
+      <div className='container'>
+        <Button  pin = 'circle-circle' className='saluteInfoButton' contentLeft={<IconInfo />} id="button2" ref={pos_y === 4 ? ref : null} outlined = {false}></Button>
+  
         {isMenuOpen && (
            <Sheet isOpen = {isMenuOpen}>
             <Body1>
@@ -178,7 +183,7 @@ const InfoButton = forwardRef(( { pos_y }, ref ) => {
             <p>2. Проверить ответ можно нажав на поле ввода "Введи ответ", написать текст и отправить его на кнопку "ОК", или словами: "Мой ответ".</p>
             <p>3. Если вы не знаете ответ, то можно нажать на кнопку "Сдаться", и выведется правильный ответ.</p>
             <h3>Желаю удачи!</h3>
-            <h4>Чтобы закрыть инструкцию, необходимо нажать "ОК".</h4>
+            <h4>Чтобы закрыть инструкцию, необходимо нажать "ОК"</h4>
             </Body1>
            </Sheet>
         )}
@@ -200,7 +205,7 @@ const LoseButton = forwardRef(( { pos_y, handleLoseClick }, ref ) => {
   } else {
   return (
     <div className="saluteLoseButton">
-      <Button contentRight = {<IconCrossCircle/>} href="#" id="button3"  ref={(pos_y === 0) ? ref : null}onClick={handleLoseClick} text = 'Сдаться'></Button>
+      <Button outlined={false} contentRight = {<IconCrossCircle/>} href="#" id="button3"  ref={(pos_y === 0) ? ref : null} text = 'Сдаться'></Button>
     </div>
   );
   }
@@ -347,7 +352,6 @@ export class App extends React.Component {
 
   add_question(action) {
     console.log('add_question', action);
-    
     fetchQuestionAndSetState((text) => {
       this._send_action_value('voice', question);
       this.setState({ 
@@ -360,7 +364,10 @@ export class App extends React.Component {
   }
 
   say_answer(action){
-    this._send_action_value('voiceAns', TrueAnswer);
+    if(TrueAnswer != 'test'){
+      this._send_action_value('voiceAns', TrueAnswer);
+    }
+    
   }
 
   async read_answer(action) {
@@ -395,30 +402,34 @@ export class App extends React.Component {
   async check_answer(action) {
     console.log('check_answer', action);
     const userAnswer = action.note || this.state.inputValue;
-    try {
-      const response = await fetch('https://4-gk.ru/api/v1/answer/check', {
-        method: 'POST',
-        body: JSON.stringify({
-          userAnswer: userAnswer,
-          correctAnswer: TrueAnswer,
-        }),
-      });
-      if (response.ok) {
-        const data = await response.json();
-        if (data.isCorrect) {
-          this.setState({ backgroundColor: 'linear-gradient(135deg, #000000 2%, #11877e 69%)' });
-          this._send_action_value('done', 'Молодец!');
+
+    if(TrueAnswer != 'test'){
+      try {
+        const response = await fetch('https://4-gk.ru/api/v1/answer/check', {
+          method: 'POST',
+          body: JSON.stringify({
+            userAnswer: userAnswer,
+            correctAnswer: TrueAnswer,
+          }),
+        });
+        if (response.ok) {
+          const data = await response.json();
+          if (data.isCorrect) {
+            this.setState({ backgroundColor: 'linear-gradient(135deg, #000000 2%, #11877e 69%)' });
+            this._send_action_value('done', 'Молодец!');
+          } else {
+            this.setState({ backgroundColor: 'linear-gradient(135deg, #000000 2%,#b42c2c 69%)' });
+            this._send_action_value('wrongAns', 'Нужно попробовать еще раз.');
+          }
+          this.setState({ inputValue: '' });
         } else {
-          this.setState({ backgroundColor: 'linear-gradient(135deg, #000000 2%,#b42c2c 69%)' });
-          this._send_action_value('wrongAns', 'Нужно попробовать ещё раз.');
+          this.setState({ inputValue: '' });
         }
-        this.setState({ inputValue: '' });
-      } else {
+      } catch (error) {
         this.setState({ inputValue: '' });
       }
-    } catch (error) {
-      this.setState({ inputValue: '' });
     }
+    
   }
 
   _send_action_value(action_id, value) {
@@ -477,15 +488,6 @@ export class App extends React.Component {
           handleKeyPress={this.handleKeyPress}
           disabled={this.state.pos_x !== 1 || this.state.pos_y !== 0} 
         />
-        <InfoButton
-          pos_x={this.state.pos_x}
-          pos_y={this.state.pos_y}
-          ref={this.anyButton}
-          onClick={() => {
-            this.anyButton.current.blur();
-          }}
-        
-        />
         <LoseButton
           pos_x={this.state.pos_x}
           pos_y={this.state.pos_y}
@@ -495,9 +497,17 @@ export class App extends React.Component {
             this.anyButton.current.blur(); 
           }}
         />
+        <InfoButton
+          pos_x={this.state.pos_x}
+          pos_y={this.state.pos_y}
+          ref={this.anyButton}
+          onClick={() => {
+            this.anyButton.current.blur();
+          }}/>
         <ButtonOutputComponent 
           onClick={() => {
-            fetchQuestionAndSetState((text) => this.setState({ outputText: text }));
+            
+            this.add_question();
             this.anyButton.current.blur(); 
           }}
           pos_x={this.state.pos_x}
